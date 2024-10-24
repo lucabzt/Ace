03da8a49210796c5dfaf3734e8cbe8978a79bd39
sebastianrogg <Sebatianwth.rogg@tum.de>
2024-10-24 21:46:07 +0200

    Folder restructure

--- Hand_Analysis/Test/test_poker_hand_analyzer.py
+++ Hand_Analysis/Test/test_poker_hand_analyzer.py
@@ -1,7 +1,7 @@
 import unittest
 
-from card import Card, Suit, Rank
-from hand_analyzer import PokerHandAnalyzer  # Adjust this import based on your actual module structure
+from Hand_Analysis.card import Card, Suit, Rank
+from Hand_Analysis.hand_analyzer import PokerHandAnalyzer  # Adjust this import based on your actual module structure
 
 
 class TestPokerHandAnalyzer(unittest.TestCase):
