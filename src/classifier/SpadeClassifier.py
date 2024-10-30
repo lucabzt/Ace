fd2654d61328b5024d1e7fbd0f70fa533ed52d9b
Sebastian Rogg <Sebastian.rogg@icloud.com>
2024-10-30 23:57:11 +0100

    Doku geht hart

--- src/classifier/SpadeClassifier.py
+++ src/classifier/SpadeClassifier.py
@@ -68,7 +68,7 @@ class SpadeClassifier(torch.nn.Module):
     def __init__(self, num_classes: int) -> None:
         super().__init__()
         features = [64, 64, 128, 256, 512]
-        num_layers = [5 for _ in range(4)]
+        num_layers = [2 for _ in range(4)]
         self.conv1 = nn.Conv2d(3, features[0], kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(features[0])
         self.relu = nn.ReLU(inplace=True)
