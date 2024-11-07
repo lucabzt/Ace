d22994f8a59dbfdc5875db2f2b4f0f9fd6eb726b
Markus <git+markus@obermui.de>
2024-11-07 21:21:14 +0100

    add classifier.py

--- src/classifier/classifier.py
+++ src/classifier/classifier.py
@@ -1,13 +1,10 @@
 # IMPORTS
 import torch
-from dataset import PlayingCardDataset
-from torch.utils.data import DataLoader
 from src.classifier.model.spadeClassifier import SpadeClassifier
 import matplotlib.pyplot as plt
 import os
 
 from src.game.resources.card import Card, Suit, Rank
-
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 torch.cuda.set_per_process_memory_fraction(0.8, device=0)
@@ -15,11 +12,11 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
 
 class Classifier:
-    def __init__(self, model: str):
-        model = SpadeClassifier().to(device)
-        model.load_state_dict(torch.load("pretrained_models/" + model, weights_only=True))
+    def __init__(self, model_path: str):
+        self.model = SpadeClassifier().to(device)
+        self.model.load_state_dict(torch.load(os.path.join("pretrained_models", model_path), weights_only=True))
 
-    def predict(self, image) -> Card:
+    def predict(self, image) -> list[Card]:
         #Scale Image
 
         #Pass to Model
@@ -29,6 +26,10 @@ class Classifier:
         outputs = self.model(images)
 
         #valuate Output
-        suit = Suit[""]
-        rank = Rank[""]
-        return Card(suit, rank)
+        cards = []
+        for output in outputs:
+            suit = Suit[""]
+            rank = Rank[""]
+            cards.append(Card(suit, rank))
+        # return cards
+        return cards
