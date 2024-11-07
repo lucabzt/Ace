5340c286928d5cb88adc3f970067c4cddaf4c9d8
Markus <git+markus@obermui.de>
2024-11-07 21:43:20 +0100

    add classifier.py

--- src/mediaplayer/sound_manager.py
+++ src/mediaplayer/sound_manager.py
@@ -4,8 +4,8 @@ from playsound3 import playsound
 # array of winner
 def play_winners_sound(winners):
     # for playing note.wav file
-    playsound('../../sounds/Poker Cards/Poker Cards/Card/Plural/FIVES.mp3')
-    playsound('../../sounds/Winning Hands/Winning Hands/full of.mp3')
+    playsound('../../assets/sounds/Poker Cards/Poker Cards/Card/Plural/FIVES.mp3')
+    playsound('../../assets/sounds/Winning Hands/Winning Hands/full of.mp3')
 
 
 # /*winner: (Name, Hand, ([Handkarten, Länge 5, sortiert - low to high], [attribut der hand]))*/
---
+++ src/spade/main.py
@@ -0,0 +1,10 @@
+from src.spade.spade import Spade
+
+
+def main():
+    spade = Spade()
+    spade.start()
+
+
+if __name__ == "__main__":
+    main()
