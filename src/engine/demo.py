73bf5ebaad67edeb1171055a87d91f6912d7ea4c
lucabozzetti <luca.bozzetti8@gmail.com>
2024-11-09 15:41:34 +0100

    added win probabilities through engine

--- src/engine/demo.py
+++ src/engine/demo.py
@@ -2,7 +2,7 @@ from table import Engine
 
 holdem_game = Engine(num_players=5, deck_type='full')
 holdem_game.add_to_hand(1, ['Td', 'Ts'])
-holdem_game.add_to_hand(2, ['Ad', 'As'])
+holdem_game.add_to_hand(2, ['5d', 'As'])
 holdem_game.next_round()
 holdem_game.next_round()
 print(holdem_game.view_table())
