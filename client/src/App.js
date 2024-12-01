95ae32dbc82db825bc0737a360160c59108f9a68
sebastianrogg <Sebastian.rogg@icloud.com>
2024-12-01 02:58:52 +0100

    Hirnblutung

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PokerGameUI from "./components/PokerGameUI";  // Import your UI component
+
+// App.js - Main App Component
+const App = () => {
+  return (
+    <div>
+      <PokerGameUI />
+    </div>
+  );
+};
+
+export default App;
